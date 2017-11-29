dfea475e29b85621ea1233302fd2878d46831d91
Froax <31766004+Froax@users.noreply.github.com>
2017-11-29 12:19:53 +0100

    Update Practice_Excersice_8_4.py

--- Practice_Excersice_8_4.py
+++ Practice_Excersice_8_4.py
@@ -1,8 +1,10 @@
 '''
 
                 [geordend]    [muteerbaar]     [iterable]    [double waarden toevoegen]
-tuple              nee            nee             ja                   nee
-dictionary         ja             ja              nee                  ja
-set                ja             nee             ja                   nee
-list               nee            ja              ja                   nee
+tuple              nee            nee             ja                   ja
+dictionary         ja             ja              ja                   ja
+set                ja             ja              ja                   nee
+list               nee            ja              ja                   ja
 '''
+
+
